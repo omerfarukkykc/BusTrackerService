40994ddcdb33cfc516ca89914300b4f8fb8ae54a
Ömer Faruk Kayıkcı <omer.twd@gmail.com>
2022-07-07 17:53:13 +0300

    Busses model added

---
+++ src/main/java/com/lepric/btservice/model/Busses.java
@@ -0,0 +1,13 @@
+package com.lepric.btservice.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "Busses")
+public class Busses {
+    
+}
